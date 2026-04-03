82ba2e5662e063e77fa6a0c46a5a97fbb1802e46
Riya72-coder <Riyakakde72@gmail.com>
2026-04-03 18:05:35 +0530

    Added logout button

--- src/Applayout/Header.jsx
+++ src/Applayout/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import{Container,Navbar,Nav} from "react-bootstrap"
+import{useNavigate} from 'react-router-dom'
 import{NavLink} from 'react-router-dom'
 
 export const Header = () => {
 
+  const Navigate= useNavigate()
+  const handleLogout=()=>{
+    localStorage.removeItem("isLogin")
+    Navigate("/login")
+
+  }
     return(
         <>
         <Navbar expand="lg" bg='dark' variant='dark'>
@@ -16,6 +23,7 @@ export const Header = () => {
             <Nav.Link as={NavLink} to="blog">Blog</Nav.Link>
             <Nav.Link as={NavLink} to="about">About</Nav.Link>
           </Nav>
+          <button onClick={handleLogout}>Logout</button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
