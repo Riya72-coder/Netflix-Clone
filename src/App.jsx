10d6a9fdb61e4a5e690eb0b61f2ba7b4aae7f3f3
Riya72-coder <Riyakakde72@gmail.com>
2026-04-03 18:04:38 +0530

    Updated login logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,36 @@ import { Applayout } from "./Applayout/Applayout"
 import { About } from "./components/About"
 import { Blog } from "./components/Blog"
 import { Home } from "./components/Home"
-import{createBrowserRouter, RouterProvider} from "react-router-dom"
+import { Login } from "./components/login"
+import { createBrowserRouter, RouterProvider, Outlet, Navigate } from "react-router-dom"
 
 function App() {
 
+  const PrivateRoutes=()=>{
+    const isLogin=localStorage.getItem("isLogin") == "true"
+    return isLogin ? <Outlet /> : <Navigate to="/login" />
+  }
+
   const router=createBrowserRouter([
+
     {
-      path:"/",
-      element:<Applayout></Applayout>,
-      children:[
-        {path:"/",element:<Home></Home>},
-        {path:"/about",element:<About></About>},
-        {path:"/blog",element:<Blog></Blog>},
-      ]
+      path:"/login", element:<Login></Login>
     },
+
+    {
+      element:<PrivateRoutes />,
+      children:[
+      {
+        path:"/",
+        element:<Applayout></Applayout>,
+        children:[
+          {path:"/",element:<Home></Home>},
+          {path:"/about",element:<About></About>},
+          {path:"/blog",element:<Blog></Blog>},
+        ]
+      },
+      ]
+    }
   ])
   
   return(
