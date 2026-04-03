57a58c398275ddd3be61e39029ded41ab6f2d62c
Riya72-coder <Riyakakde72@gmail.com>
2026-04-03 17:53:04 +0530

    Added login page logic and API handling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,21 @@
 import React, { useEffect, useState } from 'react' 
-import {getmoviedata} from '../Api/Movie'
+import {getMovieData} from '../Api/Movie'
 
-export const Home () {
-
-    console.log(getmoviedata());
+export const Home = () => {
 
     const[movie,setmovie]=useState([])
 
-    const getalldata= async()=>{
-
-        try {
-
-            const res = await getmoviedata()
-            console.log(res.data.results);
-            setmovie(res.data.results)
-
-        } catch(error) {
-            console.log(error);
-        }
     useEffect(()=>{
+        const getalldata = async () => {
+            try {
+                const res = await getMovieData()
+                console.log(res.data.results);
+                setmovie(res.data.results)
+            } catch(error) {
+                console.log(error);
+            }
+        }
+
         getalldata()
     },[])
 
@@ -30,8 +27,8 @@ export const Home () {
                 movie.map((curelm)=>(
                     <div key={curelm.id} className='col-sm-6 col-md-4 col-lg-3'>
                         <div className='card h-100'>
-                            <img src={`https://image.tmdb.org/t/p/w500${SVGFEFuncRElement.poster_path}`} className="card-img-top" alt="..."/>
-                            <div className="card-body d-flex align justify">
+                            <img src={`https://image.tmdb.org/t/p/w500${curelm.poster_path}`} className="card-img-top" alt="..."/>
+                            <div className="card-body align center">
                                 <h5 className="card-title">{curelm.original_title}</h5>
 
                                 <button className="btn btn-danger">Book Now</button>
